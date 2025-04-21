c1f41ffb0d9919a2f495386e995efd4d8ed0be01
Viktor Apuyan <157368299+viktorapuyan@users.noreply.github.com>
2025-04-22 02:03:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,6 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names=['PNEUMONIA', 'NORMAL']
+    class_names=['Pneumonia', 'Normal']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
