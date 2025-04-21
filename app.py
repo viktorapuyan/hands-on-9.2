78aef7bf398fd3ea462dac57d2793d415912610b
Viktor Apuyan <157368299+viktorapuyan@users.noreply.github.com>
2025-04-21 21:18:13 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import tensorflow as tf
+
+@st.cache_resource
+def load_model():
+  model=tf.keras.models.load_model('chest_xray_model.h5')
+  return model
+model=load_model()
+st.write("""
+# Pneumonia Detection System by Viktor"""
+)
+file=st.file_uploader("Choose xray photo from computer",type=["jpg","png"])
+
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['NORMAL','PNEUMONIA']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
