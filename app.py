6917d7cfb70de7f611aa2e755cb7ab9a6a3832f9
Viktor Apuyan <157368299+viktorapuyan@users.noreply.github.com>
2025-04-22 01:55:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,21 +14,12 @@ file=st.file_uploader("Choose xray photo from computer",type=["jpg","png"])
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
-def import_and_predict(image_data, model):
-    size = (150, 150)
-    image = ImageOps.fit(image_data, size)
-    img = np.asarray(image)
-    # Remove the extra dimension if it exists
-    if len(img.shape) == 4 and img.shape[-2] == 1:
-        img = np.squeeze(img, axis=-2)
-    # Ensure it has 3 channels if the model expects it
-    elif len(img.shape) == 3 and img.shape[-1] == 1:
-        img = np.repeat(img, 3, axis=-1)
-    elif len(img.shape) == 2:
-        img = np.stack((img,)*3, axis=-1)
-
-    img_reshape = img[np.newaxis, ...]
-    prediction = model.predict(img_reshape)
+def import_and_predict(image_data,model):
+    size=(150,150)
+    image=ImageOps.fit(image_data,size)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
     return prediction
 if file is None:
     st.text("Please upload an image file")
@@ -36,6 +27,6 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names=['PNEUMONIA', 'NORMAL']
+    class_names=['NORMAL','PNEUMONIA']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
